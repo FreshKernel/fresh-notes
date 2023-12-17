2fd469822b12c85e201f92b9ea9aefb62b982046
Ellet <hello@freshplatform.net>
2023-12-17 21:36:46 +0300

    Update pubspec.yaml version

--- lib/gen/pubspec.dart
+++ lib/gen/pubspec.dart
@@ -1,3 +1,3 @@
-const appVersion = '0.0.1-dev.2';
-const appBuildNumber = 5;
+const appVersion = '0.0.1-dev.3';
+const appBuildNumber = 6;
 const repository = 'https://github.com/freshplatform/fresh-notes';
