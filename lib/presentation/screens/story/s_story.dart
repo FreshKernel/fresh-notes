df8ecbc8e3036f4f6e061c2d937022ca25172c50
Ellet <ellet@freshplatform.net>
2023-12-21 19:02:51 +0300

    Add new Translate new strings

--- lib/presentation/screens/story/s_story.dart
+++ lib/presentation/screens/story/s_story.dart
@@ -20,37 +20,36 @@ class _StoryScreenState extends State<StoryScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = QuillController(
-      document: Document.fromJson(
-        [
-          {'insert': '\nApp development adventures !'},
-          {
-            'insert': '\n',
-            'attributes': {'header': 1}
-          },
-          {
-            'insert':
-                "\nWe are all interested in building mobile apps for iOS and Android, so we had to use macOS to write and build native iOS applications using Xcode, when using any operating system I usually take a lot of notes and I needed a notes app and I prefer it to not be a third party so I used Notes app on macOS but after updating to macOS 14.1 the notes app never working and it doesn't matter what I do I always getting crashes\n"
-          },
-          {
-            'insert': {
-              'image': Assets.images.macosNotesCrash.path,
-            },
-          },
-          {
-            'insert':
-                "it's known to be limited on AppleOS and it's closed so there is not much I can do, so I created this little app usually I prefer my repositories to the public so I decided to also publish it even though that wasn't my plan\n",
-          }
-        ],
-      ),
-      selection: const TextSelection.collapsed(offset: 0),
-    );
+    _controller = QuillController.basic();
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _controller.document = Document.fromJson(
+      [
+        {'insert': '\n${context.loc.appDevelopmentAdventuresMessage}'},
+        {
+          'insert': '\n',
+          'attributes': {'header': 1}
+        },
+        {'insert': '\n${context.loc.appDevelopmentInterestMessage}\n'},
+        {
+          'insert': {
+            'image': Assets.images.macosNotesCrash.path,
+          },
+        },
+        {
+          'insert': '${context.loc.appDevelopmentAppleMacOsIssueMessage}\n',
+        }
+      ],
+    );
   }
 
   @override
